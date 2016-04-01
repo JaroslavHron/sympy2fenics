eafaf9ea81f2b2ea75a682844395bb517071a9ca
Lizao Li <lzlarryli@gmail.com>
2016-04-01 15:44:20 -0500

    fix: gradient infer dimension. matrix div.

--- sympy2fenics.py
+++ sympy2fenics.py
@@ -15,16 +15,37 @@ def sympy2exp(exp):
     else:
         return to_ccode(exp)
 
-def grad(u, dim = 3):
+def infer_dim(u):
+    if not u.is_Matrix:
+        args = u.args
+    else:
+        args = set()
+        for exp in u[:]:
+            args = args.union(exp.args)
+    if sympy.sympify('z') in args:
+        return 3
+    elif sympy.sympify('y') in args:
+        return 2
+    else:
+        return 1    
+    
+def grad(u, dim = None):
+    if not dim:
+        dim = infer_dim(u)
+    # transpose first if it is a row vector
+    if u.is_Matrix and u.shape[0] != 1:
+        u = u.transpose()
+    # take the gradient
     if dim == 1:
-        return sympy.Matrix([u.diff('x')])
+        return sympy.Matrix([u.diff('x')]).transpose()
     elif dim == 2:
-        return sympy.Matrix([u.diff('x'), u.diff('y')])
+        return sympy.Matrix([u.diff('x'), u.diff('y')]).transpose()
     elif dim == 3:
-        return sympy.Matrix([u.diff('x'), u.diff('y'), u.diff('z')])
+        return sympy.Matrix(
+            [u.diff('x'), u.diff('y'), u.diff('z')]).transpose()
 
 def curl(u):
-    if hasattr(u, "__getitem__"):
+    if u.is_Matrix and min(u.args) == 1:
         # 3D vector curl
         return sympy.Matrix([u[2].diff('y') - u[1].diff('z'),
                              u[0].diff('z') - u[2].diff('x'),
@@ -38,16 +59,40 @@ def rot(u):
     return u[1].diff('x') - u[0].diff('y')
 
 def div(u):
-    if u.shape[0] == 2:
-        return u[0].diff('x') + u[1].diff('y')
-    elif u.shape[0] == 3:
-        return u[0].diff('x') + u[1].diff('y') + u[2].diff('z')
-
+    def vec_div(w):
+        if w.shape[0] == 2:
+            return w[0].diff('x') + w[1].diff('y')
+        elif u.shape[0] == 3:
+            return w[0].diff('x') + w[1].diff('y') + w[2].diff('z')
+    if u.shape[1] == 1:
+        # vector
+        return vec_div(u)
+    else:
+        # matrix
+        result = []
+        for i in range(u.shape[1]):
+            result.append(vec_div(u.row(i).transpose()))
+        return sympy.Matrix(result)
+    
 def sym(u):
     return (u + u.transpose()) / 2.0
 
 def tr(u):
     return u.trace()
 
-def hess(u, dim = 3):
-    return grad(grad(u, dim).transpose(), dim)
+def hess(u, dim = None):
+    return grad(grad(u, dim), dim)
+
+
+if __name__ == '__main__':
+    u2 = sympy.sympify('x*y')
+    v2 = sympy.Matrix(sympy.sympify('(x*y, y*x)'))
+    w2 = sympy.Matrix(sympy.sympify('((x*y, sin(y)), (cos(x), pi*y^2))'))
+    u3 = sympy.sympify('a*x*y*z')
+    v3 = sympy.Matrix(sympy.sympify('(x*y, y*x, x*y*z)'))
+    w3 = sympy.Matrix(sympy.sympify(
+        """
+        ((x*y,    sin(y), z), 
+         (cos(x), pi*y^2, y),
+         (z*x,    x*y*z,  x))
+        """))
