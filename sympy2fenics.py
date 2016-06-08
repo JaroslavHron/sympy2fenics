de5fff70d21f420951b54cc6b54aa081f5d9262e
Lizao Li <lzlarryli@gmail.com>
2016-06-08 13:03:15 -0500

    feat: convert to a wrapper module over sympy.

--- sympy2fenics.py
+++ sympy2fenics.py
@@ -1,10 +1,10 @@
-import sympy
+from sympy import *
 
 def sympy2exp(exp):
-    x, y, z = sympy.symbols('x[0] x[1] x[2]')    
+    x, y, z = symbols('x[0] x[1] x[2]')    
     def to_ccode(f):
         f = f.subs('x', x).subs('y', y).subs('z', z)
-        raw = sympy.printing.ccode(f)
+        raw = printing.ccode(f)
         return raw.replace("M_PI", "pi")
     if hasattr(exp, "__getitem__"):
         if exp.shape[0] == 1 or exp.shape[1] == 1:
@@ -17,9 +17,9 @@ def sympy2exp(exp):
 
 def infer_dim(u):
     atoms = u.atoms()
-    if sympy.sympify('z') in atoms:
+    if sympify('z') in atoms:
         return 3
-    elif sympy.sympify('y') in atoms:
+    elif sympify('y') in atoms:
         return 2
     else:
         return 1    
@@ -32,22 +32,22 @@ def grad(u, dim = None):
         u = u.transpose()
     # take the gradient
     if dim == 1:
-        return sympy.Matrix([u.diff('x')]).transpose()
+        return Matrix([u.diff('x')]).transpose()
     elif dim == 2:
-        return sympy.Matrix([u.diff('x'), u.diff('y')]).transpose()
+        return Matrix([u.diff('x'), u.diff('y')]).transpose()
     elif dim == 3:
-        return sympy.Matrix(
+        return Matrix(
             [u.diff('x'), u.diff('y'), u.diff('z')]).transpose()
 
 def curl(u):
     if u.is_Matrix and min(u.args) == 1:
         # 3D vector curl
-        return sympy.Matrix([u[2].diff('y') - u[1].diff('z'),
+        return Matrix([u[2].diff('y') - u[1].diff('z'),
                              u[0].diff('z') - u[2].diff('x'),
                              u[1].diff('x') - u[0].diff('y')])
     else:
         # 2D rotated gradient
-        return sympy.Matrix([u.diff('y'), -u.diff('x')])
+        return Matrix([u.diff('y'), -u.diff('x')])
 
 def rot(u):
     # 2d rot
@@ -70,7 +70,7 @@ def div(u):
         result = []
         for i in range(u.shape[1]):
             result.append(vec_div(u.row(i).transpose()))
-        return sympy.Matrix(result)
+        return Matrix(result)
     
 def sym(u):
     return (u + u.transpose()) / 2.0
@@ -81,15 +81,15 @@ def tr(u):
 def hess(u, dim = None):
     return grad(grad(u, dim), dim)
 
-eye = sympy.eye
+eye = eye
 
 if __name__ == '__main__':
-    u2 = sympy.sympify('x*y')
-    v2 = sympy.Matrix(sympy.sympify('(x*y, y*x)'))
-    w2 = sympy.Matrix(sympy.sympify('((x*y, sin(y)), (cos(x), pi*y^2))'))
-    u3 = sympy.sympify('a*x*y*z')
-    v3 = sympy.Matrix(sympy.sympify('(x*y, y*x, x*y*z)'))
-    w3 = sympy.Matrix(sympy.sympify(
+    u2 = sympify('x*y')
+    v2 = Matrix(sympify('(x*y, y*x)'))
+    w2 = Matrix(sympify('((x*y, sin(y)), (cos(x), pi*y^2))'))
+    u3 = sympify('a*x*y*z')
+    v3 = Matrix(sympify('(x*y, y*x, x*y*z)'))
+    w3 = Matrix(sympify(
         """
         ((x*y,    sin(y), z), 
          (cos(x), pi*y^2, y),
